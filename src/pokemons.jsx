52591826e0c11968e94b233e3dc7a8eae2a0742c
Diego <diego.illanes@outlook.com>
2024-07-11 22:17:40 -0400

    nombres+imagenes

---
+++ src/pokemons.jsx
@@ -0,0 +1,34 @@
+const datosPokemon = [
+    {
+        id: 1,
+        nombre: 'Gothita',
+        imagen: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/574.png',
+    },
+    {
+        id: 2,
+        nombre: 'Gothorita',
+        imagen: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/575.png',
+    },
+    {
+        id: 3,
+        nombre: 'Gothitelle',
+        imagen: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/576.png',
+    },
+    {
+        id: 4,
+        nombre: 'Ralts',
+        imagen: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/280.png',
+    },
+    {
+        id: 5,
+        nombre: 'Kirlia',
+        imagen: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/281.png',
+    },
+    {
+        id: 6,
+        nombre: 'Gardevoir',
+        imagen: 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/282.png',
+    }
+];
+  
+  export default datosPokemon;
